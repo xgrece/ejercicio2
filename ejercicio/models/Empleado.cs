c833d59a15d6bfff256e0334b8121dc37a6e18e6
malg10 <malg10@PC85>
2024-04-30 21:45:16 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio/models/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ejercicio.models
+{
+    internal class Empleado
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public string numDocumento { get; set; }
+        public Tipo_Documento tipo_Documento { get; set; } 
+        public Puesto puesto { get; set; }
+        //falta el constructor
+}
+}
