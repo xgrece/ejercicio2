6ebfce7166b7e54c733eb1121c50ba7a0ccbaf3e
valeenfiori <168212448+xgrece@users.noreply.github.com>
2024-05-07 21:45:26 -0300

    Update Empleado.cs
    s

--- ejercicio/models/Empleado.cs
+++ ejercicio/models/Empleado.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,42 +8,52 @@ namespace ejercicio.models
 {
     internal class Empleado
     {
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public DateTime FechaNacimiento { get; set; }
-        public string NumDocumento { get; set; }
-        public Tipo_Documento TipoDocumento { get; set; }
-        public Puesto Puesto { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public string numDocumento { get; set; }
+        public Tipo_Documento tipoDocumento { get; set; }
+        public Puesto puesto { get; set; }
 
         // Constructor
         public Empleado(string nombre, string apellido, DateTime fechaNacimiento, string numDocumento, Tipo_Documento tipoDocumento, Puesto puesto)
         {
-            this.Nombre = nombre;
-            this.Apellido = apellido;
-            this.FechaNacimiento = fechaNacimiento;
-            this.NumDocumento = numDocumento;
-            this.TipoDocumento = tipoDocumento;
-            this.Puesto = puesto;
-        }
-        public Empleado(string nombre, string apellido, DateTime fechaNacimiento, string numDocumento, Tipo_Documento tipoDocumento, Puesto puesto)
-        {
-            Nombre = nombre;
-            Apellido = apellido;
-            FechaNacimiento = fechaNacimiento;
-            NumDocumento = numDocumento;
-            TipoDocumento = tipoDocumento;
-            Puesto = puesto;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento = fechaNacimiento;
+            this.numDocumento = numDocumento;
+            this.tipoDocumento = tipoDocumento;
+            this.puesto = puesto;
         }
         public Empleado() { }
+
         public void MostrarInfo()
         {
-            Console.WriteLine("Información del empleado:");
-            Console.WriteLine($"Nombre: {Nombre} {Apellido}");
-            Console.WriteLine($"Fecha de nacimiento: {FechaNacimiento}");
-            Console.WriteLine($"Número de documento: {NumDocumento}");
-            Console.WriteLine($"Tipo de documento: {TipoDocumento.Tipo}");
-            Console.WriteLine($"Puesto: {Puesto.Nombre}");
-            Console.WriteLine($"Descripción del puesto: {Puesto.Descripcion}");
+            Console.WriteLine("informacion del empleado");
+            Console.WriteLine($"nombre: {nombre} {apellido} ");
+            Console.WriteLine($"fecha de nacimiento: {fechaNacimiento}");
+            Console.WriteLine($"numero de documeto: {numDocumento}");
+            Console.WriteLine($"tipo de documento: {tipoDocumento.tipo}");
+            Console.WriteLine($"puesto: {puesto.nombre}");
+            Console.WriteLine($"descripcion del puesto: {puesto.descripcion}");
+        }
+
+        //metodo para buscar un empleado por numero de doc
+        public bool sosNumeroDoc(string numeroDocumento)
+        {
+            return this.numDocumento.ToLower().Trim() == numeroDocumento.ToLower().Trim();
+        }
+        public Empleado buscarEmpleadoXdoc(string numeroDocumento)
+        {
+            foreach (Empleado empleado in empleado)
+            {
+                Console.WriteLine($"comparando con el empleado {empleado.nombre} {empleado.apellido}");
+                if (empleado.sosNumeroDoc(numeroDocumento))
+                {
+                    return empleado;
+                }
+            }
+            return null; //si no se encuentra ningun empleado
         }
     }
 }
